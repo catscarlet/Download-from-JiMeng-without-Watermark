d53062ab259ac2b54c6f7f8245e6418b89418327
catscarlet <i@catscarlet.com>
2026-02-25 15:58:04 +0800

    Support Image Download

--- Download-from-JiMeng-without-Watermark.user.js
+++ Download-from-JiMeng-without-Watermark.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
-// @name            从即梦AI下载无水印视频 Download Origin Video from JiMeng without Watermark
-// @name:en         Download Origin Video from JiMeng without Watermark 从即梦AI下载无水印视频
+// @name            从即梦AI下载无水印视频和图片 Download Origin Video and Image from JiMeng without Watermark
+// @name:en         Download Origin Video and Image from JiMeng without Watermark 从即梦AI下载无水印视频和图片
 // @namespace       https://github.com/catscarlet/Download-from-JiMeng-without-Watermark
-// @description     从即梦AI（jimeng.jianying.com）下载无水印视频. Download Origin Video from jimeng.jianying.com without Watermark
-// @description:en  Download Origin Video from jimeng.jianying.com without Watermark. 从即梦AI（jimeng.jianying.com）下载无水印视频
-// @version         0.0.4
+// @description     从即梦AI（jimeng.jianying.com）下载无水印视频和图片. Download Origin Video and Image from jimeng.jianying.com without Watermark
+// @description:en  Download Origin Video and Image from jimeng.jianying.com without Watermark. 从即梦AI（jimeng.jianying.com）下载无水印视频和图片
+// @version         0.1.0
 // @author          catscarlet
 // @license         GNU Affero General Public License v3.0
 // @match           https://jimeng.jianying.com/ai-tool/*
@@ -37,7 +37,7 @@
 
                         if (!checkBtn1) {
                             const promptNode = grandParent1.parentNode.previousSibling.querySelector('.prompt-P_8aF8');
-                            const fileName = getFileName(promptNode);
+                            const fileName = getVideoFileName(promptNode);
                             const downloadVideoButton = generateDownloadVideoButton(fileName);
                             downloadVideoButton.addEventListener('click', async () => {
                                 getCrossOriginVideo(videoWrapper, downloadVideoButton, fileName);
@@ -53,7 +53,7 @@
                         if (!checkBtn2) {
 
                             const promptNode = grandParent2.querySelector('.prompt-value-p42svP');
-                            const fileName = getFileName(promptNode);
+                            const fileName = getVideoFileName(promptNode);
                             const downloadVideoButton = generateDownloadVideoButton(fileName);
 
                             downloadVideoButton.addEventListener('click', async () => {
@@ -66,6 +66,25 @@
                     } else {
                     }
 
+                });
+
+                document.querySelectorAll('.preview-k4e1UX').forEach((imgElement, imgElementIndex) => {
+                    const container = imgElement.parentNode;
+
+                    const hasButton = imgElement.hasAttribute('data-preview-button-added');
+                    if (hasButton) {
+                        return;
+                    }
+
+                    const fileName = getImageFileName();
+                    const downloadImageButton = generateDownloadImageButton(fileName);
+                    downloadImageButton.addEventListener('click', async (e) => {
+                        e.stopPropagation();
+                        getCrossOriginImage(imgElement, downloadImageButton, fileName);
+                    });
+                    container.appendChild(downloadImageButton);
+
+                    imgElement.setAttribute('data-preview-button-added', 'true');
                 });
             });
         }});
@@ -116,6 +135,43 @@ async function getCrossOriginVideo(videoWrapper, downloadVideoButton, fileName) 
     }
 }
 
+async function getCrossOriginImage(imgElement, downloadImageButton, fileName) {
+    const btnOriginStyle = {};
+
+    btnOriginStyle.cursor = downloadImageButton.style.cursor;
+    btnOriginStyle.backgroundColor = downloadImageButton.style.backgroundColor;
+
+    downloadImageButton.style.cursor = 'not-allowed';
+    downloadImageButton.style.backgroundColor = 'grey';
+
+    const fileUrl = imgElement.src;
+
+    try {
+        const response = await fetch(fileUrl, {mode: 'cors'});
+        const blob = await response.blob();
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = fileName;
+        a.style.display = 'none';
+        document.body.appendChild(a);
+        setTimeout(() => {
+            a.click();
+        }, 10);
+        setTimeout(() => {
+            URL.revokeObjectURL(url);
+            document.body.removeChild(a);
+            downloadImageButton.style.cursor = btnOriginStyle.cursor;
+            downloadImageButton.style.backgroundColor = btnOriginStyle.backgroundColor;
+        }, 1000);
+    } catch (error) {
+        alert('加载失败，请确保服务器开启了 CORS 支持。');
+
+        downloadImageButton.style.cursor = btnOriginStyle.cursor;
+        downloadImageButton.style.backgroundColor = btnOriginStyle.backgroundColor;
+    }
+}
+
 function generateDownloadVideoButton(fileName) {
     const downloadVideoButton = document.createElement('div');
     downloadVideoButton.className = 'noWaterMarkDownloadVideoButton';
@@ -132,11 +188,42 @@ function generateDownloadVideoButton(fileName) {
     return downloadVideoButton;
 }
 
-function getFileName(promptNode) {
+function generateDownloadImageButton(fileName) {
+    const downloadImageButton = document.createElement('div');
+    downloadImageButton.className = 'noWaterMarkDownloadImageButton';
+    downloadImageButton.title = '下载文件名为「' + fileName + '」的图片文件';
+    downloadImageButton.textContent = '下载预览图片';
+    downloadImageButton.style.backgroundColor = 'blue';
+    downloadImageButton.style.color = 'white';
+    downloadImageButton.style.position = 'absolute';
+    downloadImageButton.style.top = '0.5em';
+    downloadImageButton.style.left = '0.5em';
+    downloadImageButton.style.padding = '8px 12px';
+    downloadImageButton.style.borderRadius = '4px';
+    downloadImageButton.style.cursor = 'pointer';
+
+    return downloadImageButton;
+}
+
+function getVideoFileName(promptNode) {
     let fileName;
 
     if (promptNode && promptNode.textContent != '无提示词') {
         fileName = promptNode.textContent;
+    } else {
+        fileName = '无提示词-' + getYmdHMS();
+    }
+
+    return fileName;
+}
+
+function getImageFileName() {
+    const promptNode = document.querySelector('.prompt-value-p42svP');
+
+    let fileName;
+
+    if (promptNode && promptNode.textContent != '无提示词') {
+        fileName = '即梦无水印-' + promptNode.textContent + '-' + getYmdHMS();;
     } else {
         fileName = '无提示词-' + getYmdHMS();
     }
