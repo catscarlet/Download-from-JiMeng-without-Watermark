7fe8dd2d4ac7c24975e087c37b7522680793eb04
catscarlet <i@catscarlet.com>
2026-08-31 23:46:01 +0800

    Update querySelector to match JiMeng update.

--- Download-from-JiMeng-without-Watermark.user.js
+++ Download-from-JiMeng-without-Watermark.user.js
@@ -16,13 +16,13 @@
 
 const workspacePrefixOn = 1; //Set 0 to turn off workspacePrefix. The workspacePrefix feature only works when you download in `/ai-tool/generate?workspace`. Does not works in `/ai-tool/asset`
 
-const imagePreviewSelectors = '.preview-D8m4nj';
-const videoDivSelectors = '.video-wrapper-Qs3DdX';
-const videoRecordContentClassname = 'video-record-content-Fi81fu';
-const videoPromptSelectors = '.prompt-YBbGAQ';
-const videoDivSelectorsClassname = 'video-element-WzNSkE';
-const videoPublishButtonSelectors = '.publish-button-tugZ4y';
-const promptValueSelectors = '.prompt-value-gaqnj0';
+const imagePreviewSelectors = '.preview-hQ6jgj';
+const videoDivSelectors = '.video-wrapper-eGa56T';
+const videoRecordContentClassname = 'video-record-content-gs4TIE';
+const videoPromptSelectors = '.prompt-j8MAI4';
+const videoDivSelectorsClassname = 'video-element-E9ReM6';
+const videoPublishButtonSelectors = '.publish-button-zkCSyG';
+const promptValueSelectors = '.prompt-value-UdWTeN';
 
 (function() {
     'use strict';
